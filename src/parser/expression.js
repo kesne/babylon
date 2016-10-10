0fb758f8d550c67ece24769ba8aa6deab3692453
Jordan Gensler <jordan.gensler@airbnb.com>
2016-10-10 11:40:32 -0700

    [import()] Throwing on more than one argument

--- src/parser/expression.js
+++ src/parser/expression.js
@@ -300,6 +300,9 @@ pp.parseSubscripts = function (base, startPos, startLoc, noCalls) {
       let node = this.startNodeAt(startPos, startLoc);
       node.callee = base;
       node.arguments = this.parseCallExpressionArguments(tt.parenR, possibleAsync);
+      if (node.callee.type === "Import" && node.arguments.length > 1) {
+        this.raise(node.start, "import() only accepts one argument");
+      }
       base = this.finishNode(node, "CallExpression");
 
       if (possibleAsync && this.shouldParseAsyncArrow()) {
